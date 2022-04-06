4074b83efe7b511e347524290e6d59855e12d0ff
tohidshabanloo <tshabanloo@hotmail.com>
2022-04-06 18:15:33 +0430

    created

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Gallery from "./components/Gallery/Gallery";
+import Image from "./components/Image/Image";
+import List from "./components/list/List";
+
+function App() {
+  return (
+    <div className="App">
+      <Image />
+      <List />
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/list/List.js
@@ -0,0 +1,6 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import './List.css'
+
+
+const 
