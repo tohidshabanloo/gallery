4bd45f3d39a26526ccd75c26befc74beea2be74e
tohidshabanloo <tshabanloo@hotmail.com>
2022-04-07 11:11:33 +0430

    finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Gallery from "./components/Gallery/Gallery";
-import Image from "./components/Image/Image";
-import List from "./components/list/List";
+// import Image from "./components/Image/Image";
+// import List from "./components/list/List";
 
 function App() {
   return (
     <div className="App">
-      <Image />
-      <List />
+      {/* <Image />
+      <List /> */}
       <Gallery />
     </div>
   );
--- src/components/list/List.js
+++
@@ -1,6 +0,0 @@
-import { useState } from "react";
-import { useEffect } from "react";
-import './List.css'
-
-
-const 
